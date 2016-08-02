e67b8ca2407ce76b03968738aa08c1f6fd84323a
tiagoferreira <si.tiagoferreira@gmail.com>
2016-08-01 23:41:35 -0300

    read files using stream

--- src/main/java/br/com/duck/archive/processor/util/FileReader.java
+++ src/main/java/br/com/duck/archive/processor/util/FileReader.java
@@ -1,10 +1,19 @@
 package br.com.duck.archive.processor.util;
 
-import java.nio.file.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class FileReader {
 
-	public File readFile(String path) {
-		return null;
+	public Stream<String> readFile(String path) throws IOException{
+		return Files.lines(Paths.get(path));
+	}
+	
+	public static void main(String[] args) throws IOException{
+		FileReader reader = new FileReader();
+		Stream<String> lines = reader.readFile("/home/tiago/bagacera.dat");
+		lines.forEach(System.out::println);
 	}
 }
---
+++ src/test/java/br/com/duck/archive/processor/util/test/FileReaderTest.java
@@ -0,0 +1,22 @@
+package br.com.duck.archive.processor.util.test;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.duck.archive.processor.util.FileReader;
+
+public class FileReaderTest {
+
+	FileReader fileReader = new FileReader();
+	
+	
+	@Test
+	public void testReadFile() throws IOException{
+		Stream<String> lines = fileReader.readFile("/home/tiago/bagacera.dat");
+		Assert.assertNotNull(lines);
+		Assert.assertTrue(lines.count() > 0);
+	}
+}
