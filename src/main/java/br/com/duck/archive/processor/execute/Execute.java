d4dda683f5c373f18e1fbf9252355a0cce65111a
tiagoferreira <si.tiagoferreira@gmail.com>
2016-08-31 22:36:08 -0300

    add method start o main

--- src/main/java/br/com/duck/archive/processor/execute/Execute.java
+++ src/main/java/br/com/duck/archive/processor/execute/Execute.java
@@ -1,8 +1,11 @@
 package br.com.duck.archive.processor.execute;
 
+import br.com.duck.archive.processor.proccess.FileProcessor;
+
 public class Execute {
 
 	public static void main(String[] args) {
-		
+			FileProcessor processor = new FileProcessor();
+		    processor.startProcess();
 	}
 }
