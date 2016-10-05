63c04ee2c432d592b04a102ec32f91d464a47738
tiagoferreira <si.tiagoferreira@gmail.com>
2016-10-04 21:28:59 -0300

    adjust enum of LayoutTypes

--- src/main/java/br/com/duck/archive/processor/types/LayoutType.java
+++ src/main/java/br/com/duck/archive/processor/types/LayoutType.java
@@ -1,5 +1,17 @@
 package br.com.duck.archive.processor.types;
 
 public enum LayoutType {
-	CUSTOMER, SALES, SALESMAN
-}
+	CUSTOMER(002), SALES(003), SALESMAN(001);
+	
+	private int code;
+	
+	private LayoutType(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	
+}	
--- src/main/java/br/com/duck/archive/processor/util/ConverterSelector.java
+++ src/main/java/br/com/duck/archive/processor/util/ConverterSelector.java
@@ -1,15 +1,20 @@
 package br.com.duck.archive.processor.util;
 
+import br.com.duck.archive.processor.converters.LineConverter;
+import br.com.duck.archive.processor.types.LayoutType;
+
 public class ConverterSelector {
+	
+	private  LineConverter converter;
 
 	 public void selectConverter(String string) {
 	        int type = Integer.parseInt(string.substring(0, 3));
 	        String line = "";
-	        if (type == 001) {
+	        if (type == LayoutType.SALESMAN.getCode()) {
 	            line = string.substring(4, string.length());
-	        } else if (type == 002) {
+	        } else if (type ==  LayoutType.CUSTOMER.getCode()) {
 	            line = string.substring(4, string.length());
-	        } else if (type == 003) {
+	        } else if (type ==  LayoutType.SALES.getCode()) {
 	            line = string.substring(4, string.length());
 	        } else {
 	            System.err.println("Type of data not suported :-<");
