d06cbf2acd86d44e607f717c95634704f937e0e1
tiagoferreira <si.tiagoferreira@gmail.com>
2016-10-07 00:20:56 -0300

    refctoring converter selector

--- src/main/java/br/com/duck/archive/processor/util/ConverterSelector.java
+++ src/main/java/br/com/duck/archive/processor/util/ConverterSelector.java
@@ -1,23 +1,44 @@
 package br.com.duck.archive.processor.util;
 
-import br.com.duck.archive.processor.converters.LineConverter;
+import br.com.duck.archive.processor.converters.CustomerConverter;
+import br.com.duck.archive.processor.converters.SalesConverter;
+import br.com.duck.archive.processor.converters.SalesmanConverter;
+import br.com.duck.archive.processor.dao.CustomerDAO;
+import br.com.duck.archive.processor.dao.SalesDAO;
+import br.com.duck.archive.processor.dao.SalesmanDAO;
 import br.com.duck.archive.processor.types.LayoutType;
 
 public class ConverterSelector {
 	
-	private  LineConverter converter;
 
 	 public void selectConverter(String string) {
 	        int type = Integer.parseInt(string.substring(0, 3));
-	        String line = "";
 	        if (type == LayoutType.SALESMAN.getCode()) {
-	            line = string.substring(4, string.length());
+	            addSalesman(string);
 	        } else if (type ==  LayoutType.CUSTOMER.getCode()) {
-	            line = string.substring(4, string.length());
+	            addCustomer(string);
 	        } else if (type ==  LayoutType.SALES.getCode()) {
-	            line = string.substring(4, string.length());
+	            addSales(string);
 	        } else {
 	            System.err.println("Type of data not suported :-<");
 	        }
 	    }
+
+	private void addSales(String string) {
+		String line;
+		line = string.substring(4, string.length());
+		SalesDAO.add(new SalesConverter().converter(line));
+	}
+
+	private void addCustomer(String string) {
+		String line;
+		line = string.substring(4, string.length());
+		CustomerDAO.add(new CustomerConverter().converter(line));
+	}
+
+	private void addSalesman(String string) {
+		String line;
+		line = string.substring(4, string.length());
+		SalesmanDAO.add(new SalesmanConverter().converter(line));
+	}
 }
