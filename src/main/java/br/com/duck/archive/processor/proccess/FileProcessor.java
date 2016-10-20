4e60d88a19232ca626550b1f4df5ff5d2de7a031
tiagoferreira <si.tiagoferreira@gmail.com>
2016-10-19 22:43:14 -0200

    process file

--- src/main/java/br/com/duck/archive/processor/proccess/FileProcessor.java
+++ src/main/java/br/com/duck/archive/processor/proccess/FileProcessor.java
@@ -3,15 +3,20 @@ package br.com.duck.archive.processor.proccess;
 import java.io.IOException;
 import java.util.stream.Stream;
 
+import br.com.duck.archive.processor.util.ConverterSelector;
 import br.com.duck.archive.processor.util.FileReader;
 
 public class FileProcessor {
 	
 	FileReader fileReader = new FileReader();
+	ConverterSelector converter = new ConverterSelector();
 	
 	public void startProcess() {
 		try {
 			Stream<String> contentsOfFile = fileReader.readFile("bagacera.dat", true);
+			contentsOfFile.forEach( a -> converter.selectConverter(a));
+			
+			
 			
 		} catch (IOException e) {
 			e.printStackTrace();
