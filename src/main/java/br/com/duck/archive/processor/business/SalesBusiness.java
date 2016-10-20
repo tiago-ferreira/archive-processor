4559eea54dfceb42d32f04b9eff945358c1ec00e
tiagoferreira <si.tiagoferreira@gmail.com>
2016-10-20 21:08:04 -0200

    add business package

---
+++ src/main/java/br/com/duck/archive/processor/business/SalesBusiness.java
@@ -0,0 +1,12 @@
+package br.com.duck.archive.processor.business;
+
+import br.com.duck.archive.processor.model.Salesman;
+
+public class SalesBusiness {
+
+	public Salesman worseSalesmanEver() {
+		Salesman toReturn = null;
+		
+		return toReturn;
+	}
+}
