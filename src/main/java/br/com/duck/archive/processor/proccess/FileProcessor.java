2144f92b3d734dd9e355fb0fe74459c83d7659ec
tiagoferreira <si.tiagoferreira@gmail.com>
2016-11-04 00:24:33 -0200

    method to return quantity of custumers e salesman on input file

--- src/main/java/br/com/duck/archive/processor/proccess/FileProcessor.java
+++ src/main/java/br/com/duck/archive/processor/proccess/FileProcessor.java
@@ -5,6 +5,8 @@ import java.math.BigDecimal;
 import java.util.stream.Stream;
 
 import br.com.duck.archive.processor.business.SalesBusiness;
+import br.com.duck.archive.processor.dao.CustomerDAO;
+import br.com.duck.archive.processor.dao.SalesmanDAO;
 import br.com.duck.archive.processor.model.Sales;
 import br.com.duck.archive.processor.util.ConverterSelector;
 import br.com.duck.archive.processor.util.FileReader;
@@ -22,6 +24,8 @@ public class FileProcessor {
 			BigDecimal amountOfSales = SalesBusiness.amountOfSales();
 			System.out.println(mostExpensiveSale);
 			System.out.println(amountOfSales);
+			System.out.println(CustomerDAO.getCustomers().size());
+			System.out.println(SalesmanDAO.getSalesmans().size());
 			
 		} catch (IOException e) {
 			e.printStackTrace();
